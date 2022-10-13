eb695e5c3dea9d294d3ed135b68f15a3b49d677b
calebtripp <calebtripp@outlook.com>
2022-10-13 11:36:53 -0400

    Exercise 2 Complete

--- APICurrentForecast/APICurrentForecast/Program.cs
+++ APICurrentForecast/APICurrentForecast/Program.cs
@@ -9,86 +9,63 @@ using System.Text.Json;
 namespace APICurrentForecast
 {
     internal class Program
-    {   
-            static void Main(string[] args)
-            {
-                var client = new HttpClient();
-                var city = "Harrisburg";
-                var state = "PA";
-                var country = "USA";
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var city = "Harrisburg";
+            var state = "PA";
+            var country = "USA";
+
 
             //GEOLOCATOR//////////////////////////
-
-            var locatorUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={1}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}&units=imperial";
+            var locatorUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={1}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}";
             var apiGeoResponse = client.GetStringAsync(locatorUrl).Result;
+
             var lat = "";
             var lon = "";
-
-            JArray parsedApi = JArray.Parse(apiGeoResponse);      
-
+            JArray parsedApi = JArray.Parse(apiGeoResponse);
             foreach (JObject jObject in parsedApi)
             {
                 lat = $"{(string)jObject["lat"]}";
                 lon = $"{(string)jObject["lon"]}";
             }
-             
-            //using (JsonDocument apiGeoResponse = JsonDocument.Parse(json, options))
-            //{
-            //    int sumOfAllTemperatures = 0;
-            //    int count = 0;
-
-            //    foreach (JsonElement element in document.RootElement.EnumerateArray())
-            //    {
-            //        DateTimeOffset date = element.GetProperty("date").GetDateTimeOffset();
-            //        (...)
-
-            // these two need the array response parsed first. 
-            //    var lat = JArray.Parse(apiGeoResponse).GetValue("lat").ToString();
-            // var lon = JArray.Parse(apiGeoResponse).GetValue("lon").ToString();         
 
 
-            //FORECAST///////////////////////////
-
-            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}";
+            //FORECAST////////////////////////////
+            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}&units=imperial";
             var apiWeaResponse = client.GetStringAsync(weatherUrl).Result;
-            Console.WriteLine(apiWeaResponse);
-            //will need to parse out current forecast. 
 
-            //
-            //  JArray FromObject(object o)
-            //  var forecast = 
+            var main = JObject.Parse(apiWeaResponse).GetValue("main").ToString();
+                var temp = JObject.Parse(main).GetValue("temp").ToString();
+                var tempMin = JObject.Parse(main).GetValue("temp_min").ToString();
+                var tempMax = JObject.Parse(main).GetValue("temp_max").ToString();
+                var feelsLike = JObject.Parse(main).GetValue("feels_like").ToString();
+                var humidity = JObject.Parse(main).GetValue("humidity").ToString();
 
-            //Console.WriteLine($"{apiWeaResponse}");
+            var wind = JObject.Parse(apiWeaResponse).GetValue("wind").ToString();
+                var speed = JObject.Parse(wind).GetValue("speed").ToString();
 
+            var weather = JObject.Parse(apiWeaResponse).GetValue("weather").ToString();
+                var conditions = "";
+
+            JArray parsedWeather = JArray.Parse(weather);
+            foreach (JObject jObject in parsedWeather)
+            {
+                conditions = $"{(string)jObject["description"]}";
+            }
+
+            var forecast = $"The current weather forecast in {city} is: \n" +
+                $"{conditions}\n" +
+                $"Temperature: {temp}\n" +
+                $"Low: {tempMin}\n" +
+                $"High: {tempMax}\n" +
+                $"Feels Like: {feelsLike}\n" +
+                $"Humidity: {humidity}\n" +
+                $"Wind Speed: {speed}\n";
+
+            Console.WriteLine(forecast);
+            //Not clean, need to move forecast and geolocator to classes and find a way to more efficiently parse. Serialize? JSON document?
         }
-
     }
 }
-//need to add other api
-
-
-//C Cre
-//R
-//U
-//D
-
-
-//Array - Array.parse
-//obj obj . parse
-
-
-
-//api key 269fcd68b6d3d0d501d5f58d3d61fc61
-
-
-
-//Use thex
-//OpenWeatherMap APIx
-//to find out what the current weather forecast is!
-//Create an OpenWeatherMap account to get a free API Keyx
-//Add the NewtonSoft.Json Nuget Package to your Console Appx
-//Figure out how to get the CURRENT weather for the City you specify in degrees fahrenheit
-//Use this site for extra help:
-//https://openweathermap.org/current
-//Creatively display &organize the response
-//Hint: For more info on Units of Measurement visit:
