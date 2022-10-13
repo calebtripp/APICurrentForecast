865b1a6c889d7f99d570e936952ceb33035c317b
calebtripp <calebtripp@outlook.com>
2022-10-13 10:14:31 -0400

    Progress Save, Geo Working

--- APICurrentForecast/APICurrentForecast/Program.cs
+++ APICurrentForecast/APICurrentForecast/Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
 using System.Net;
+using System.Text.Json;
 
 namespace APICurrentForecast
 {
@@ -19,29 +20,48 @@ namespace APICurrentForecast
             //GEOLOCATOR//////////////////////////
 
             var locatorUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={1}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}&units=imperial";
-            // var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q={london},{state code},{country code}&limit={1}&appid={269fcd68b6d3d0d501d5f58d3d61fc61}&units=imperial";
-            //var weatherUrl = $"https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}";
-            var apiResponse = client.GetStringAsync(locatorUrl).Result;
-            var lat = JArray.Parse(apiResponse).GetValue("lat").ToString();
-                        //locator response is an array
+            var apiGeoResponse = client.GetStringAsync(locatorUrl).Result;
+            var lat = "";
+            var lon = "";
 
+            JArray parsedApi = JArray.Parse(apiGeoResponse);      
+
+            foreach (JObject jObject in parsedApi)
+            {
+                lat = $"{(string)jObject["lat"]}";
+                lon = $"{(string)jObject["lon"]}";
+            }
+             
+            //using (JsonDocument apiGeoResponse = JsonDocument.Parse(json, options))
+            //{
+            //    int sumOfAllTemperatures = 0;
+            //    int count = 0;
+
+            //    foreach (JsonElement element in document.RootElement.EnumerateArray())
+            //    {
+            //        DateTimeOffset date = element.GetProperty("date").GetDateTimeOffset();
+            //        (...)
+
+            // these two need the array response parsed first. 
+            //    var lat = JArray.Parse(apiGeoResponse).GetValue("lat").ToString();
+            // var lon = JArray.Parse(apiGeoResponse).GetValue("lon").ToString();         
 
 
             //FORECAST///////////////////////////
 
-            //  var weatherUrl =  https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}";
+            var apiWeaResponse = client.GetStringAsync(weatherUrl).Result;
+            Console.WriteLine(apiWeaResponse);
+            //will need to parse out current forecast. 
 
-
-
-            // var lon = JArray.Parse(apiResponse).GetValue("lon").ToString();  
             //
             //  JArray FromObject(object o)
             //  var forecast = 
 
-            Console.WriteLine($"{apiResponse}");
+            //Console.WriteLine($"{apiWeaResponse}");
 
-            }
-        
+        }
+
     }
 }
 //need to add other api
