1e707da211c6743257d4154deb637a9c3e9a39f0
calebtripp <calebtripp@outlook.com>
2022-10-22 14:34:02 -0400

    Userinput started, needs minor exception handling

--- APICurrentForecast/APICurrentForecast/Program.cs
+++ APICurrentForecast/APICurrentForecast/Program.cs
@@ -1,27 +1,35 @@
 ﻿using System.Runtime.Intrinsics.X86;
-using static System.Net.WebRequestMethods;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
 using System.Net;
 using System.Text.Json;
+using System.IO;
 
 namespace APICurrentForecast
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {           
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
 
+            Console.WriteLine("Please enter the name of your city:");
+            string userCity = Console.ReadLine();
+
+            Console.WriteLine("Please enter the two letter abbreviation for your state:");
+            string userState = Console.ReadLine();                  
+            
 
             var client = new HttpClient();
-            var city = "Harrisburg";
-            var state = "PA";
-            var country = "USA";
+            var city = userCity;
+            var state = userState;
+            string country = "USA";
 
 
-            //GEOLOCATOR//////////////////////////
-            var locatorUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={1}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}";
+            //GEOLOCATOR////////////////////////// Move to it's own class?
+            var locatorUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={1}&appid={APIKey}";
             var apiGeoResponse = client.GetStringAsync(locatorUrl).Result;
 
             var lat = "";
@@ -30,26 +38,46 @@ namespace APICurrentForecast
             foreach (JObject jObject in parsedApi)
             {
                 lat = $"{(string)jObject["lat"]}";
-                lon = $"{(string)jObject["lon"]}";
-            }
+                lon = $"{(string)jObject["lon"]}";            }
 
 
-            //FORECAST////////////////////////////
-            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"269fcd68b6d3d0d501d5f58d3d61fc61"}&units=imperial";
+            //FORECAST//////////////////////////// Move to it's own class?
+            //var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}&units=imperial";
+            //var apiWeaResponse = client.GetStringAsync(weatherUrl).Result;
+
+
+            //var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}&units=imperial";
+            //var apiWeaResponse = client.GetStringAsync(weatherUrl).Result;
+
+            //try
+            //{
+            //    var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}&units=imperial";
+            //    var apiWeaResponse = client.GetStringAsync(weatherUrl).Result;
+            //}
+
+            //catch (System.AggregateException)
+            //{
+
+            //    Console.WriteLine("We were not able to find that location in the USA, please confirm your city and state spelling and try again ");
+            //}
+
+            var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}&units=imperial";
             var apiWeaResponse = client.GetStringAsync(weatherUrl).Result;
 
+
+
             var main = JObject.Parse(apiWeaResponse).GetValue("main").ToString();
-                var temp = JObject.Parse(main).GetValue("temp").ToString();
-                var tempMin = JObject.Parse(main).GetValue("temp_min").ToString();
-                var tempMax = JObject.Parse(main).GetValue("temp_max").ToString();
-                var feelsLike = JObject.Parse(main).GetValue("feels_like").ToString();
-                var humidity = JObject.Parse(main).GetValue("humidity").ToString();
+            var temp = JObject.Parse(main).GetValue("temp").ToString();
+            var tempMin = JObject.Parse(main).GetValue("temp_min").ToString();
+            var tempMax = JObject.Parse(main).GetValue("temp_max").ToString();
+            var feelsLike = JObject.Parse(main).GetValue("feels_like").ToString();
+            var humidity = JObject.Parse(main).GetValue("humidity").ToString();
 
             var wind = JObject.Parse(apiWeaResponse).GetValue("wind").ToString();
-                var speed = JObject.Parse(wind).GetValue("speed").ToString();
+            var speed = JObject.Parse(wind).GetValue("speed").ToString();
 
             var weather = JObject.Parse(apiWeaResponse).GetValue("weather").ToString();
-                var conditions = "";
+            var conditions = "";
 
             JArray parsedWeather = JArray.Parse(weather);
             foreach (JObject jObject in parsedWeather)
@@ -57,17 +85,16 @@ namespace APICurrentForecast
                 conditions = $"{(string)jObject["description"]}";
             }
 
-            var forecast = $"The current weather forecast in {city} is: \n" +
+            var forecast = $"\nThe current weather forecast in {userCity} is:\n" +
                 $"{conditions}\n" +
                 $"Temperature: {temp}\n" +
-                $"Low: {tempMin}\n" +
-                $"High: {tempMax}\n" +
                 $"Feels Like: {feelsLike}\n" +
-                $"Humidity: {humidity}\n" +
-                $"Wind Speed: {speed}\n";
+                $"Today's Low: {tempMin}\n" +
+                $"Today's High: {tempMax}\n" +
+                $"Wind Speed: {speed}\n" +
+                $"Humidity: {humidity}\n";
 
             Console.WriteLine(forecast);
-            //Not clean, need to move forecast and geolocator to classes and find a way to more efficiently parse. Serialize? JSON document?
         }
     }
 }
